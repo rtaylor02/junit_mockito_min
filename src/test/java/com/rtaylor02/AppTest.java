41d967556b5e459d236377053c1ca16cbc901c67
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2024-09-10 19:30:34 +0100

    Init commit

---
+++ src/test/java/com/rtaylor02/AppTest.java
@@ -0,0 +1,22 @@
+package com.rtaylor02;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @InjectMocks
+    private App systemUnderTest;
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
